b3166dc85e532218fffc4437b07ddb7cc315d55a
unknown <rradzvil@ucsc.edu>
2020-06-15 14:20:09 -0700

    Project boost complete

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,49 +1,96 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
-    AudioSource rocketThrust;
+    AudioSource audioSource;
 
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
-    
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip explosion;
+    [SerializeField] AudioClip success;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem explosionParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
+
+    bool collisionOn = true;
+    bool thrusting = false;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        rocketThrust = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (UnityEngine.Debug.isDebugBuild)
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                collisionOn = !collisionOn;
+            }
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                LoadNextLevel();
+            }
+        }
+        if (state == State.Alive)
+        {
+            ThrustInput();
+            RotateInput();
+        }
     }
 
-    private void Thrust()
+    private void ThrustInput()
     {
 
         float thrustThisFrame = mainThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
-            if (!rocketThrust.isPlaying)
+            print(state);
+            if (thrusting == false) 
             {
-                rocketThrust.Play();
+                mainEngineParticles.Play();
+                thrusting = true;
             }
+            ApplyThrust(thrustThisFrame);
         }
         else
         {
-            rocketThrust.Stop();
+            audioSource.Stop();
+            if (thrusting == true)
+            {
+                mainEngineParticles.Stop();
+                thrusting = false;
+            }
         }
     }
 
-    private void Rotate()
+    private void ApplyThrust(float thrustThisFrame)
+    {
+        rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        //mainEngineParticles.Play();
+    }
+
+    private void RotateInput()
     {
         rigidBody.freezeRotation = true; // take control of rotation
 
@@ -57,23 +104,62 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
-
         rigidBody.freezeRotation = false; // grant physics control of rotation
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive || collisionOn == false) { return; } // Ignore collisions when dead
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("Ok");
-                break;
-            case "Fuel":
-                print("Ok");
+                break; // Do nothing
+            case "Finish":
+                successSequence();
                 break;
             default:
-                print("Dead");
+                deathSequence();
                 break;
         }
+    }
+
+    private void successSequence()
+    {
+        successParticles.Play();
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        Invoke("LoadNextLevel", levelLoadDelay); // parameterize time
+    }
+
+    private void deathSequence()
+    {
+        explosionParticles.Play();
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(explosion);
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+        state = State.Alive;
+        audioSource.Stop();
+        explosionParticles.Stop();
+    }
+
+    private void LoadNextLevel()
+    {
+        int sceneNum = SceneManager.GetActiveScene().buildIndex;
+        print(sceneNum);
+        print(SceneManager.sceneCountInBuildSettings);
+        int nextScene = sceneNum >= SceneManager.sceneCountInBuildSettings - 1 ? 0 : sceneNum + 1;
+        print(nextScene);
+        SceneManager.LoadScene(nextScene);
+        state = State.Alive;
+        audioSource.Stop();
+        successParticles.Stop();
     }
 }
---
+++ Assets/Rotator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Rotator : MonoBehaviour
+{
+
+    [SerializeField] float xAngle = 0.001f;
+    [SerializeField] float yAngle = 0.0f;
+    [SerializeField] float zAngle = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xAngle, yAngle, zAngle);
+    }
+}
